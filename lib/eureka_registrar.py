933782c32b87fa90cd56cf3329bab9265a9c040e
1193868 <scott.capista@cvshealth.com>
2019-11-21 09:29:31 -0500

    automated update

--- lib/eureka_registrar.py
+++ lib/eureka_registrar.py
@@ -97,7 +97,7 @@ def get_access_token(credentials):
 		return None
 	req = urllib.request.Request(access_token_uri)
 	cred_str = client_id + ":" + client_secret
-	cred_byte = cred.encode('utf-8')
+	cred_byte = cred_str.encode('utf-8')
 	req.add_header('Authorization', 'Basic ' + base64.b64encode(cred_byte))
 	body = "grant_type=client_credentials"
 	response = json.load(urllib.request.urlopen(req, data=body, **urlargs))
